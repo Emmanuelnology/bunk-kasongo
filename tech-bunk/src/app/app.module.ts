e70d80417ecb49121ec21946c8cb369c42a3a601
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2019-01-14 12:54:19 +0000

    created weather component

--- tech-bunk/src/app/app.module.ts
+++ tech-bunk/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from "@angular/router";
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "weather", component:  WeatherComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
